c0562b936505bc6f2493df25774d5e78dadf3bbc
PietiK <pieti.kinnunen@gmail.com>
2021-02-28 23:25:22 +0200

    SQLite, tietokanta.db

---
+++ main/Tietokanta.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.SQLException;  
+
+public class Tietokanta {
+  public static void connect() {  
+    Connection conn = null;  
+    try {  
+        //Tietokannan osoite
+        String url = "jdbc:sqlite:C:tietokanta.db";  
+        //Luo yhteys tietokantaan 
+        conn = DriverManager.getConnection(url);  
+          
+        System.out.println("Yhteys tietokantaan muodostettu");  
+          
+    } catch (SQLException e) {  
+        System.out.println(e.getMessage());  
+    } finally {  
+        try {  
+            if (conn != null) {  
+                conn.close();  
+            }  
+        } catch (SQLException ex) {  
+            System.out.println(ex.getMessage());  
+        }  
+    }  
+}  
+  public static void main(String[] args) {  
+      connect();  
+  }  
+}
